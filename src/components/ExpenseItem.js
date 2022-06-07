40d09f60ed24c5543626a3642d17c865b3ff4e03
xBasox <borko12329@gmail.com>
2022-06-07 12:31:59 +0300

    refactoring code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,16 @@
-import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-const ExpenseItem = (props) => {  
-
+const ExpenseItem = (props) => {
   return (
-    <div className='expense-item'>
-      <ExpenseDate date={props.date}/>
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
       <h2>{props.title}</h2>
-      <div className='epense-item__description'>
-        <div className='expense-item__price'>{"$"+props.amount}</div>
+      <div className="epense-item__description">
+        <div className="expense-item__price">{"$" + props.amount}</div>
       </div>
     </div>
   );
-}
+};
 
 export default ExpenseItem;
